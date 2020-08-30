92312cc06c384e2cb4efac30f393c14b7764ed33
cuongnt <a4k49040@gmail.com>
2020-08-30 11:29:50 +0700

    update

--- research/deeplab/datasets/data_generator.py
+++ research/deeplab/datasets/data_generator.py
@@ -99,10 +99,20 @@ _ADE20K_INFORMATION = DatasetDescriptor(
     ignore_label=0,
 )
 
+_HAIR_INFORMATION = DatasetDescriptor(
+    splits_to_sizes={
+        'train': 18000,  # num of samples in images/training
+        'val': 6000,  # num of samples in images/validation
+    },
+    num_classes=2,
+    ignore_label=0,
+)
+
 _DATASETS_INFORMATION = {
     'cityscapes': _CITYSCAPES_INFORMATION,
     'pascal_voc_seg': _PASCAL_VOC_SEG_INFORMATION,
     'ade20k': _ADE20K_INFORMATION,
+    'hair': _HAIR_INFORMATION,
 }
 
 # Default file pattern of TFRecord of TensorFlow Example.
